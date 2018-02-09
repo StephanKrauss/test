e621479356e02c2f4833720d4ab9177b8fa7f26b
LSL\stephan.krauss <stephan.krauss@lsl.de>
2018-02-09 15:20:18 +0100

    Branch [master]: Test PhpMd

--- public/index.php
+++ public/index.php
@@ -1,13 +1,4 @@
 <?php
-    // error_reporting(E_ERROR | E_WARNING | E_PARSE);
+    include_once ('../vendor/autoload.php');
 
-    // $username = 'joe';        // in real life this would be from $_SESSION
 
-    // and then much further down in the code...
-
-    if ($username) {            // typo, $usernmae expands to null
-        echo "Logged in";
-    }
-    else {
-        echo "Please log in...";
-    }
