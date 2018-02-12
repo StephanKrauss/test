d70d680cab5cdbec15955adbd25896e5e0e5ed31
LSL\stephan.krauss <stephan.krauss@lsl.de>
2018-02-12 13:28:19 +0100

    Branch [master]: Branch [develop]: cherry 2

---
+++ src/app/sk/cherry2.php
@@ -0,0 +1,13 @@
+<?php
+    /**
+     * Beschreibung
+     *
+     * @subpackage Task ???
+     * @author stephan.krauss
+     * @date 12.02.2018
+     * @time 13:27
+     * @link Bsp. Aufruf eines Controller
+     */
+
+    $wert = 111;
+    $wert = 222;
